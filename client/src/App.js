e8ed9148b5057272ede895d0ead1edc6e9c6e9eb
Joseph Nielsen <josephmnielsen@gmail.com>
2021-04-14 13:11:27 -0700

    app page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Auth from './pages/Auth'
 
 
 
 
 function App() {
   return (
-
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/profile' exact component={Profile} />
+          <Route path='/auth' exact component={Auth} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
