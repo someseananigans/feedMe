ba6eadab5509a52b28915b81a46ed8175267ea41
Joseph Nielsen <josephmnielsen@gmail.com>
2021-04-13 17:55:34 -0700

    post"card"

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import { Button } from '@material-ui/core'
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 import ChatIcon from '@material-ui/icons/ChatBubbleOutline'; 
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 const useStyles = makeStyles((theme) => ({
