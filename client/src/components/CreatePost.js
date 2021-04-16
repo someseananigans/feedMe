dd452fd077fba25d11477375dd06eca4b40ab280
Sean Nguyen <nsean321@gmail.com>
2021-04-16 10:22:54 -0700

    icon and styling

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -12,39 +12,55 @@ import Post from '../utils/Post'
 import { storage } from '../utils/firebase'
 import Fab from "@material-ui/core/Fab";
 import AddPhotoAlternateIcon from "@material-ui/icons/AddPhotoAlternate";
-
-
+import CloseIcon from '@material-ui/icons/Close';
+import { Divider } from '@material-ui/core';
 
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
-    maxWidth: 500,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    maxWidth: 500
   },
   title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
+    fontSize: 14
   },
   flexColumn: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center'
   },
-  input: {
+  fileInput: {
     display: 'none'
+  },
+  relative: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    position: "relative",
+    justifyContent: "center"
+  },
+  absolute: {
+    width: "50px",
+    height: "50px",
+    position: "absolute",
+    top: "13px",
+    right: 0,
+    bottom: '3px',
+    left: "13px"
+  },
+  padding: {
+    padding: 0
+  },
+  toFrame: {
+    width: "90%"
   }
 })
 
 const CreatePost = () => {
-
   const classes = useStyles()
+
+  const [display, setDisplay] = useState(false) 
+  const [mainState, setState] = useState("initial")
   const [postState, setPostState] = useState({
     body: '',
     image: '',
@@ -53,9 +69,8 @@ const CreatePost = () => {
 
   const handleInputChange = ({ target }) => {
     setPostState({ ...postState, [target.name]: target.value })
+    setDisplay(true)
   }
-
-  // firebase integration needed for getting image url and setting img within the state
 
   const handleCreatePost = event => {
     event.preventDefault()
@@ -65,20 +80,16 @@ const CreatePost = () => {
     })
       .then(({ data: post }) => {
         console.log(post)
-        const posts = [...postState.posts]
-        posts.push(post)
-        setPostState({ ...postState, posts, body: '', image: '' })
+        setPostState({ ...postState, body: '', image: '' })
       })
       .catch(err => console.error(err))
   }
 
-  // const [image, setImage] = useState(null)
   const handleFileChange = event => {
     event.preventDefault()
     // **** can put conditional on file.size to put a limit on the size of a file
     const file = event.target.files[0]
     const imgName = "Gram" + Date.now()
-    // imgName is the image ref inside the storage ref and you are putting the image file grabbed from the "image" state
     const uploadTask = storage.ref(`images/${imgName}`).put(file)
     uploadTask.on(
       "state_changed",
@@ -91,33 +102,69 @@ const CreatePost = () => {
           .getDownloadURL()
           .then(firebaseUrl => {
             setPostState({ ...postState, image: firebaseUrl })
+            setState("uploaded")
+            setDisplay(true)
           })
       }
     )
-    console.log(file)
   }
 
+  const cardReset = event => {
+    setState("initial")
+    setPostState({ ...postState, body: '', image: '' })
+    setDisplay(false)
+  }
+
+  const renderAddImage = () => {
+    return (
+      <div>
+        <input
+          accept="image/*"
+          className={classes.fileInput}
+          id="contained-button-file"
+          multiple
+          type="file"
+          onChange={handleFileChange}
+        />
+        <label htmlFor="contained-button-file">
+          <Fab component="span" className={classes.button}>
+            <AddPhotoAlternateIcon />
+          </Fab>
+        </label>
+      </div>
+    )
+  }
+
+  const renderUploaded = () => {
+    return (
+      <div>
+        <img
+          width="100%"
+          src={postState.image}
+          alt="your image"
+        />
+      </div>
+    )
+  }
 
   return (
     <>
       <Card className={classes.root} variant="outlined">
-        <CardContent>
+        <CardContent className={classes.padding}>
           <form className={classes.flexColumn} noValidate autoComplete="off">
-          <h1>Create A Post</h1>
-            <input
-              accept="image/*"
-              className={classes.input}
-              id="contained-button-file"
-              multiple
-              type="file"
-              onChange={handleFileChange}
-            />
-            <label htmlFor="contained-button-file">
-              <Fab component="span" className={classes.button}>
-                <AddPhotoAlternateIcon />
+            <div className={classes.relative}>
+
+              <h1>Create A Post</h1>
+              {display &&
+              <Fab component="span" onClick={cardReset} className={classes.absolute}>
+                <CloseIcon />
               </Fab>
-            </label>
-            <TextField label="Write a Caption" name="body" onChange={handleInputChange} />
+              }
+            </div>
+
+            {(mainState == "initial" && renderAddImage()) || (mainState == "uploaded" && renderUploaded())}
+
+            <TextField className={classes.toFrame} label="Write a Caption" name="body" onChange={handleInputChange} value={postState.body} />
             <br />
             <Button variant="contained" onClick={handleCreatePost}>Publish</Button>
           </form>
