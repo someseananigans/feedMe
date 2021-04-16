a1e8f31f2c6f88e36dd1d387d14287d52d45f2fc
Sean Nguyen <nsean321@gmail.com>
2021-04-16 14:02:12 -0700

    added more export for organization

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,5 @@ export { default as Auth } from "./auth/Auth"
 export { default as Post } from "./posts/Post"
 export { default as CreatePost } from "./CreatePost"
 export { default as Suggested } from './Suggested'
+export { default as ProfileModal } from './modals/ProfileModal'
+export { default as EditProfile } from './EditProfile'
