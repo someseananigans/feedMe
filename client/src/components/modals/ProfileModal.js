172fa343eae0ba11314474e94a4bded88ee7923f
Sean Nguyen <nsean321@gmail.com>
2021-04-16 14:03:34 -0700

    Profile Modal Setup

---
+++ client/src/components/modals/ProfileModal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+const ProfileModal = () => {
+
+  const [open, setOpen] = useState(false)
+
+  const toggleOpen = () => {
+    setOpen(!open)
+  }
+
+  const modal = {
+
+  }
+
+  return (
+    <div>
+
+      
+
+    </div>
+  )
+}
+
+export default ProfileModal
