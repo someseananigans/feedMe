c78a6d5e424209d77729086ef7952710d51a92d3
Sean Nguyen <nsean321@gmail.com>
2021-04-19 13:09:38 -0700

    Locked implentation

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,26 @@ import {
   Switch,
   Route
 } from 'react-router-dom'
-import {Home, Profile, Login} from './pages'
+import { Home, Profile, Login } from './pages'
+import { LockedView } from './utils'
 
 
 function App() {
   return (
     <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/profile' exact component={Profile} />
-          <Route path='/auth' exact component={Login} />
-        </Switch>
+      <Switch>
+        <Route exact path='/'>
+          <LockedView>
+            <Home />
+          </LockedView>
+        </Route>
+        <Route exact path='/profile'>
+          <LockedView>
+            <Profile />
+          </LockedView>
+        </Route>
+        <Route path='/auth' exact component={Login} />
+      </Switch>
     </Router>
   )
 }
