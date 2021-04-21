cd6d5cf0845ef5bcfd409c19b9469b603fc77616
Sean Nguyen <nsean321@gmail.com>
2021-04-20 18:34:53 -0700

    Remove unused imports

--- client/src/components/grams/Comment.js
+++ client/src/components/grams/Comment.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import {
-  CardHeader, CardContent, CardActions, IconButton,
-  Button, TextField, Avatar, Typography, Box, Checkbox
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 
 
 const Comment = (props) => {
