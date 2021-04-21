59f7a6e7d2ff60b7477528944116ca1cc3ff0dc2
Kyle <kohlwiler@yahoo.com>
2021-04-21 14:36:44 -0700

    deleted space

--- client/src/components/Suggested.js
+++ client/src/components/Suggested.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { Avatar, CardHeader, Typography, IconButton } from '@material-ui/core';
 import { Link } from 'react-router-dom'
-
 import User from '../utils/User.js'
 
 const useStyles = makeStyles((theme) => ({
