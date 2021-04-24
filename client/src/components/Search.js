de94f012e7761c81382976d1ddde7116180943e2
Kyle <kohlwiler@yahoo.com>
2021-04-24 11:41:39 -0700

    moved search to center

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -16,6 +16,7 @@ const useStyles = makeStyles((theme) => ({
         // height: 400,
         // overflow: 'scroll'
       },
+      justifyContent: 'center'
     },
     suggestions: {
       marginLeft: 20,
