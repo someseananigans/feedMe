4e269c0beac8159e887447aee0ad52b7287d8d80
Sean Nguyen <nsean321@gmail.com>
2021-05-16 04:29:01 -0700

    organize

--- client/src/components/auth/Auth.js
+++ client/src/components/auth/Auth.js
@@ -14,7 +14,6 @@ function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-
 const Auth = () => {
   const classes = useStyles()
 
@@ -35,13 +34,16 @@ const Auth = () => {
   return (
     <Container component="main" maxWidth="xs">
       <Paper className={classes.paper} elevation={3}>
+
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
-        <form className={classes.form} onSubmit={handleSubmit}>
 
+        <Typography variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+
+        <form className={classes.form} onSubmit={handleSubmit}>
           <Grid container spacing={2}>
+
             {isSignup && (
               <>
                 <div className={classes.input} >
@@ -50,40 +52,48 @@ const Auth = () => {
 
                 <div className={classes.input}>
                   <Input name="email" validate label="Email Address" value={registerState.email} handleChange={handleInputChange} type="email" autocomplete="off" />
-
                 </div>
               </>
             )}
 
             <div className={classes.input}>
               <Input name="username" label="username" value={isSignup ? registerState.username : loginState.username} handleChange={handleInputChange} type="username" autocomplete="off" />
-
             </div>
 
             <div className={classes.input}>
               <Input name="password" label="Password" value={isSignup ? registerState.password : loginState.password} handleChange={handleInputChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
-
             </div>
+
           </Grid>
+
           <Button type="submit" onClick={(event) => handleSubmit(event)} fullWidth variant="contained" color="primary" className={classes.submit}>
             {isSignup ? 'Sign Up' : "Sign In"}
           </Button>
+
           <Grid container justify="flex-end">
             <Grid item>
+
               <Button onClick={switchMode}>
                 {isSignup ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}
               </Button>
+
             </Grid>
-
           </Grid>
-        </form>
-      </Paper>
-      <div className={classes.snack}>
 
+        </form>
+
+      </Paper>
+
+      <div className={classes.snack}>
         {snackAlert && Object.entries(snackAlert).map(([key, value]) =>
           <>
-            <Snackbar className={classes.child} open={snackAlert[key] ? true : false} autoHideDuration={600000} onClose={handleClose} ClickAwayListenerProps={{ onClickAway: () => null }}
-            >
+            <Snackbar
+              ClickAwayListenerProps={{ onClickAway: () => null }}
+              open={snackAlert[key] ? true : false}
+              autoHideDuration={600000}
+              className={classes.child}
+              onClose={handleClose} >
+
               <Alert style={{ flex: 1 }} onClose={handleClose} severity="error">
                 {value}
               </Alert>
@@ -92,6 +102,7 @@ const Auth = () => {
           </>
         )}
       </div>
+
     </Container >
   )
 }
