692e54542f4df3a8a85db1369ead12e4c2c927d9
Sean Nguyen <nsean321@gmail.com>
2021-05-16 04:31:44 -0700

    Added like/comment/update functionalities

--- client/src/utils/FollowContext.js
+++ client/src/utils/FollowContext.js
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import { User } from '../utils'
-
-// this is a pseudo FAKE Context not a TRUE Context
+import { Comment as Cmnt, User } from '../utils'
 
 const FollowContext = () => {
 
+  // start follow functionality
   const [following, setFollowing] = useState({
     users: []
   })
@@ -26,13 +25,82 @@ const FollowContext = () => {
     setFollowAction(usersFollow.indexOf(focusedUser) !== -1 ? 'following' : 'follow')
   }
 
+  // end follow functionality
+
+  // start like functionality
+
+  const [likeAction, setLikeAction] = useState(false)
+  const [likeCount, setLikeCount] = useState(0)
+
+  const likeCheck = (likedByUsers, currentUser) => {
+    setLikeAction(likedByUsers.indexOf(currentUser.user._id) !== -1 ? 'unlike' : 'like')
+  }
+
+  const handleLike = async (postId) => {
+    await User.touchPost({
+      type: likeAction,
+      post_id: postId
+    })
+      .then()
+      .catch(err => console.log(err))
+    setLikeCount(likeAction === 'like' ? (likeCount + 1) : (likeCount - 1))
+    setLikeAction(likeAction === 'like' ? 'unlike' : 'like')
+  }
+
+  // end like functionality
+
+  // start comment functionality
+
+  const [cmntList, setCmntList] = useState([])
+
+  const [comment, setComment] = useState({
+    body: '',
+    post_id: ''
+  })
+
+  const handleCommentInput = ({ target }) => {
+    setComment({ ...comment, body: target.value, post_id: target.id })
+  }
+
+  const handleComment = () => {
+    Cmnt.create({
+      comment: comment.body,
+      post_id: comment.post_id
+    })
+      .then(({ data: cmnt }) => {
+        setComment({ ...comment, body: '', post_id: '' })
+        setUpdate('Need Update')
+      })
+      .catch(err => console.error(err))
+  }
+
+  const [commentList, setCommentList] = useState([])
+
+  // end comment functionality
+
+  // start update functionality
+
+  const [update, setUpdate] = useState('')
+
+  // end update functionality 
+
+
+
   return {
-    following,
-    setFollowing,
+    following, setFollowing,
     handleFollow,
-    followAction,
-    setFollowAction,
-    followCheck
+    followAction, setFollowAction,
+    followCheck,
+    likeAction, setLikeAction,
+    likeCount, setLikeCount,
+    likeCheck,
+    handleLike,
+    comment, setComment,
+    commentList, setCommentList,
+    cmntList, setCmntList,
+    handleComment,
+    handleCommentInput,
+    update, setUpdate
   }
 
 }
