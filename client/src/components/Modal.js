16d97da550f8d2ca806e3c75b54caec48cd2d62d
Sean Nguyen <nsean321@gmail.com>
2021-05-16 04:27:51 -0700

    Ready for deletion

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,71 +1,73 @@
 
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Modal from '@material-ui/core/Modal';
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
+// // is this used????
 
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+// function rand() {
+//   return Math.round(Math.random() * 20) - 10;
+// }
 
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
+// function getModalStyle() {
+//   const top = 50 + rand();
+//   const left = 50 + rand();
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: 'absolute',
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
+//   return {
+//     top: `${top}%`,
+//     left: `${left}%`,
+//     transform: `translate(-${top}%, -${left}%)`,
+//   };
+// }
 
-export default function SimpleModal() {
-  const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = React.useState(false);
+// const useStyles = makeStyles((theme) => ({
+//   paper: {
+//     position: 'absolute',
+//     width: 400,
+//     backgroundColor: theme.palette.background.paper,
+//     border: '2px solid #000',
+//     boxShadow: theme.shadows[5],
+//     padding: theme.spacing(2, 4, 3),
+//   },
+// }));
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+// export default function SimpleModal() {
+//   const classes = useStyles();
+//   // getModalStyle is not a pure function, we roll the style only on the first render
+//   const [modalStyle] = React.useState(getModalStyle);
+//   const [open, setOpen] = React.useState(false);
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+//   const handleOpen = () => {
+//     setOpen(true);
+//   };
 
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Text in a modal</h2>
-      <p id="simple-modal-description">
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
-      <SimpleModal />
-    </div>
-  );
+//   const handleClose = () => {
+//     setOpen(false);
+//   };
 
-  return (
-    <div>
-      <button type="button" onClick={handleOpen}>
-        Open Modal
-      </button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        {body}
-      </Modal>
-    </div>
-  );
-}
+//   const body = (
+//     <div style={modalStyle} className={classes.paper}>
+//       <h2 id="simple-modal-title">Text in a modal</h2>
+//       <p id="simple-modal-description">
+//         Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+//       </p>
+//       <SimpleModal />
+//     </div>
+//   );
+
+//   return (
+//     <div>
+//       <button type="button" onClick={handleOpen}>
+//         Open Modal
+//       </button>
+//       <Modal
+//         open={open}
+//         onClose={handleClose}
+//         aria-labelledby="simple-modal-title"
+//         aria-describedby="simple-modal-description"
+//       >
+//         {body}
+//       </Modal>
+//     </div>
+//   );
+// }
