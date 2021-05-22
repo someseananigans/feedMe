7237ed2d34fc5ee79ef022e06c9d9be1682fbe86
Sean Nguyen <nsean321@gmail.com>
2021-05-22 03:47:41 -0700

    seet loading spinner for following posts

--- client/src/components/grams/Cards.js
+++ client/src/components/grams/Cards.js
@@ -14,12 +14,25 @@ const Cards = () => {
   const [value, setValue] = useState(0);
   const [view, setView] = useState(true)
   const [loading, setLoading] = useState(true)
+  const [loading2, setLoading2] = useState(true)
   const [currentUser, setCurrentUser] = useState({
     // user: {}
   })
   const handleChange = (event, newValue) => {
     setValue(newValue);
     handleView()
+    console.log(followingPosts.length)
+    if (newValue === 1 && followingPosts.length < 1) {
+      setLoading2(true)
+      Post.getFollowing()
+        .then(({ data: fgrams }) => {
+          setFollowingPosts(fgrams)
+        })
+        .catch(err => console.error(err))
+      setTimeout(() => {
+        setLoading2(false)
+      }, 3000);
+    }
   };
 
   const handleView = () => {
@@ -28,45 +41,51 @@ const Cards = () => {
 
   const RenderRecent = ({ show }) => {
     return (
-      <Box xs={12} xl={12} lg={12} md={12} style={{ display: show }}>
-        { postState.length && postState.map(post =>
-          <Card
-            postId={post._id}
-            userId={post.user._id}
-            username={post.user.username}
-            profile={post.user.profile}
-            image={post.image}
-            caption={post.body}
-            created_on={post.created_On}
-            likedByNumber={post.liked_by.length ? post.liked_by.length : 0}
-            likedByUsers={post.liked_by}
-            currentUser={currentUser}
-          />
-        )
-        }
-      </Box>
+      <>
+        { loading && <LinearProgress />}
+        <Box xs={12} xl={12} lg={12} md={12} style={{ display: show }}>
+          {postState.length && postState.map(post =>
+            <Card
+              postId={post._id}
+              userId={post.user._id}
+              username={post.user.username}
+              profile={post.user.profile}
+              image={post.image}
+              caption={post.body}
+              created_on={post.created_On}
+              likedByNumber={post.liked_by.length ? post.liked_by.length : 0}
+              likedByUsers={post.liked_by}
+              currentUser={currentUser}
+            />
+          )
+          }
+        </Box>
+      </>
     )
   }
 
-  const RenderFollow = () => {
+  const RenderFollow = ({ show }) => {
     return (
-      <Box xs={12} xl={12} lg={12} md={12} >
-        { followingPosts.length && followingPosts.map(gram =>
-          <Card
-            postId={gram._id}
-            userId={gram.user._id}
-            username={gram.user.username}
-            profile={gram.user.profile}
-            image={gram.image}
-            caption={gram.body}
-            created_on={gram.created_On}
-            likedByNumber={gram.liked_by.length ? gram.liked_by.length : 0}
-            likedByUsers={gram.liked_by}
-            currentUser={currentUser}
-          />
-        )
-        }
-      </Box>
+      <>
+        { loading2 && <LinearProgress />}
+        <Box xs={12} xl={12} lg={12} md={12} style={{ display: show }}>
+          {followingPosts.length && followingPosts.map(gram =>
+            <Card
+              postId={gram._id}
+              userId={gram.user._id}
+              username={gram.user.username}
+              profile={gram.user.profile}
+              image={gram.image}
+              caption={gram.body}
+              created_on={gram.created_On}
+              likedByNumber={gram.liked_by.length ? gram.liked_by.length : 0}
+              likedByUsers={gram.liked_by}
+              currentUser={currentUser}
+            />
+          )
+          }
+        </Box>
+      </>
     )
   }
 
@@ -79,11 +98,11 @@ const Cards = () => {
         setPostState(grams)
       })
       .catch(err => console.error(err))
-    Post.getFollowing()
-      .then(({ data: fgrams }) => {
-        setFollowingPosts(fgrams)
-      })
-      .catch(err => console.error(err))
+    // Post.getFollowing()
+    //   .then(({ data: fgrams }) => {
+    //     setFollowingPosts(fgrams)
+    //   })
+    //   .catch(err => console.error(err))
     setTimeout(() => {
       setLoading(false)
     }, 3000);
@@ -113,9 +132,9 @@ const Cards = () => {
           </Tabs>
         </Paper>
       </Box>
-      {loading && <LinearProgress />}
+
       {/* if view thent Recent is rendered (only after loading 300s is complete) */}
-      {view ? (<RenderRecent show={loading && 'none'} />) : (<RenderFollow />)}
+      {view ? (<RenderRecent show={loading && 'none'} />) : (<RenderFollow show={loading2 && 'none'} />)}
 
     </>
   )
